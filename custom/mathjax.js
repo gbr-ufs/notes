588b2e7bedf4486387c4c5765519c47690f8bbec
gbr-ufs <gbr-ufs@users.noreply.github.com>
2025-07-31 21:15:11 +0000

    deploy: 08dca820c9e59a55a61996cfe3a63945d849af73

--- custom/mathjax.js
+++ custom/mathjax.js
@@ -4,8 +4,10 @@ window.Mathjax = {
 };
 
 (function () {
-    var script = document.createElement('script');
+    let script = document.createElement('script');
+
     script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
     script.async = true;
+
     document.head.appendChild(script);
 })();
